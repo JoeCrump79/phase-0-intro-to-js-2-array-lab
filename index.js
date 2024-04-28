bccaac9c0a61b6ba99da7d4f71953afcd0b69628
Joseph Crump <josephcrump79@yahoo.com>
2024-04-27 20:12:45 -0500

    First release of Hello World!

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(name) { cats.push(name) }
+function destructivelyPrependCat(name) { cats.unshift(name) }
+function destructivelyRemoveLastCat() { cats.pop() }
+function destructivelyRemoveFirstCat() {cats.shift() }
+function appendCat(name) { 
+    let myCats = [...cats] 
+    myCats.push (name)
+    return myCats
+}
+function prependCat(name) { 
+    let myCats = [...cats] 
+    myCats.unshift (name)
+    return myCats
+}
+function removeLastCat() { 
+    let myCats = [...cats] 
+    myCats.pop ()
+    return myCats
+}
+function removeFirstCat() { 
+    let myCats = [...cats] 
+    myCats.shift ()
+    return myCats
+}
